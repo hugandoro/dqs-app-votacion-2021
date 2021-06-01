d5c5add6acc9bb27eacfa51f3f3c09f2dec5e6fb
Hugo Andres Orozco <hugandoro@gmail.com>
2021-06-01 10:18:09 -0500

    Banner y CSS

--- view/portada/home.view.php
+++ view/portada/home.view.php
@@ -1,9 +1,11 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-xs-12" align="center">
-        <iframe width="100%" height="600" src="https://www.youtube.com/embed/SpP-HgtMXXo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <a href="https://presupuestoparticipativo.dosquebradas.gov.co/app_votacion_2021/?c=Preinscripcion&a=Index&token="><img src="assets/img/banner_pre.jpg" width="100%"></a>
     </div>
 </div>
 
 <!--<div class="row">
-    <div class="col-sm-12"></div>
+    <div class="col-lg-12 col-md-12 col-xs-12" align="center">
+        <iframe width="100%" height="600" src="https://www.youtube.com/embed/SpP-HgtMXXo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
 </div>-->
